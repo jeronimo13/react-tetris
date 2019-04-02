c77e9cab24aabe36c2166a97c1a534a585d31212
herman.havrysh <herman.havrysh@sandbx.co>
2019-04-02 07:00:15 +0300

    Updates

---
+++ precache-manifest.b864fb1fcfade1504b3807522a23457c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d3654f2c28baa8ab860",
+    "url": "/react-tetris/static/js/runtime~main.98c4844d.js"
+  },
+  {
+    "revision": "b1a52456fb18b6707cda",
+    "url": "/react-tetris/static/js/main.9ea1ff8e.chunk.js"
+  },
+  {
+    "revision": "3ee318356542d0dd4b8f",
+    "url": "/react-tetris/static/js/2.9d4dd8ad.chunk.js"
+  },
+  {
+    "revision": "b1a52456fb18b6707cda",
+    "url": "/react-tetris/static/css/main.6d0d32c9.chunk.css"
+  },
+  {
+    "revision": "a124e28d628525b9a01d53f0ee9097e5",
+    "url": "/react-tetris/index.html"
+  }
+];
